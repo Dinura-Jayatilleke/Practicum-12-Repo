dec508b1121b631f58aa405e74af545b5e326772
Dinura Jayatilleke <you@example.com>
2024-02-13 09:23:47 -0500

    code yes or no

--- main.java
+++ main.java
@@ -12,11 +12,26 @@
 // "User entered no"
 //
 // (TO COMPLETE)
-public class Main {
+import java.util.*;
+public class main {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
 
-      System.out.println("Hello World!");
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter 'Yes' or 'No':");
+      String ans = input.next();
 
+      if (ans = String.equalsIgnoreCase('YES') ) {
+        System.out.println("User entered YES");
+      }
+      else if (ans = String.equalsIgnoreCase('NO') ) {
+        System.out.println("User entered No");
+        
+      }else{
+        System.out.println("Invalid reply");
+      }
 
     }
 
